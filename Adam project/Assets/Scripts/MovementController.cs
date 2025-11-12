724b2974a6a474d1a26d3fb67a03141c89af0080
test <236991818+testlgi@users.noreply.github.com>
2025-11-12 21:14:49 +0100

    movement2

--- Adam project/Assets/Scripts/MovementController.cs
+++ Adam project/Assets/Scripts/MovementController.cs
@@ -16,4 +16,34 @@ public class MovementController : MonoBehaviour
         rigidbody = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if (Input.GetKey(inputUp)) {
+            SetDirection(Vector2.up);
+        } else if (Input.GetKey(inputDown)) {
+            SetDirection(Vector2.down);
+        } else if (Input.GetKey(inputLeft)) {
+            SetDirection(Vector2.left);
+        } else if (Input.GetKey(inputRight)) {
+            SetDirection(Vector2.right);
+        } else {
+            SetDirection(Vector2.zero);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        Vector2 translation = direction * speed * Time.fixedDeltaTime;
+
+        rigidbody.MovePosition(position + translation);
+    }
+
+    private void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection;
+
+        //...
+    }
+
 }
