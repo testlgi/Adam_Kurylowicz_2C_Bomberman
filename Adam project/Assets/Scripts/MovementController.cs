b34482e56f08ffd7c298ff23252da5ea38839049
test <236991818+testlgi@users.noreply.github.com>
2025-11-12 21:01:24 +0100

    movement

--- Adam project/Assets/Scripts/MovementController.cs
+++ Adam project/Assets/Scripts/MovementController.cs
@@ -1,18 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public new Rigidbody2D rigidbody { get; private set; }
+    private Vector2 direction = Vector2.down;
+    public float speed = 5f;
+
+    public KeyCode inputUp = KeyCode.W;
+    public KeyCode inputDown = KeyCode.S;
+    public KeyCode inputLeft = KeyCode.A;
+    public KeyCode inputRight = KeyCode.D;
+
+    private void Awake()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
