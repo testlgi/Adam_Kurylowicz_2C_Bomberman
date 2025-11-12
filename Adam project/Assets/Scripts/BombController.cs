380582eede22bc35312f190808e03dec32fec292
test <236991818+testlgi@users.noreply.github.com>
2025-11-12 22:48:38 +0100

    bombpush

---
+++ Adam project/Assets/Scripts/BombController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public KeyCode inputKey = KeyCode.Space;
+    public GameObject bombPrefab;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+    private int bombsRemaining;
+
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+
+    private void Update()
+    {
+        if (bombsRemaining > 0 && Input.GetKeyDown(inputKey)) {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+
+        GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        yield return new WaitForSeconds(bombFuseTime);
+
+        Destroy(bomb);
+        bombsRemaining++;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bomb")) {
+            other.isTrigger = false;
+        }
+    }
+
+}
